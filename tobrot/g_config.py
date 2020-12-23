fced5de95285ef1542d9448fb31aa3f0c0f3fd24
cyrus-op <76032422+cyrus-op@users.noreply.github.com>
2020-12-23 07:14:18 -0800

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -3,10 +3,10 @@ from tobrot.sample_config import Config
 
 class Config(Config):
 	TG_BOT_TOKEN= "1268fill---your----data"
-	APP_ID = 1373938
-	API_HASH = "fill--your--data"
-	OWNER_ID = "" #ID of bot owner
-	AUTH_CHANNEL = [-10082786282972]
-	DESTINATION_FOLDER = "HelloMan" #Name of your folder read readme
+	APP_ID = 2526751
+	API_HASH = "9d95cd9e9bede6332e1da19276d9ad63"
+	OWNER_ID = "1197580900" 
+	AUTH_CHANNEL = [-1001434651700]
+	DESTINATION_FOLDER = "HelloMan" 
 	RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.fill---your---data----7gGcxnYr71RBuoMVlzhAM_-fil your davxrOpIuvZ-S5lo4YzhYqP:"Bearer","refresh_token":"1//0gnh0j3fUzRITCgYIARAAGBASNwF-L9IrbbYT1QkJg6UCEo3AksHDp6DqUBrhcTBIwUWtlgmbnBQPZKvSAEOTl098VTt8zEojRnY","expiry":"2fill--your--data-07-13T10:4fillyourdata758376Z"}"""
 	#fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
